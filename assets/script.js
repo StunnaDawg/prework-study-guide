ca610739ba7e33b1595d938fb41f7881b5280ea5
StunnaDawg <jonsonallen9@gmail.com>
2022-12-25 22:03:06 -0400

    for loop js

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+let topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+for (var x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+}
+
+/*
+
+
+let topic = "Git";
+
+if (topic === "HTML") {
+    console.log("Let's study HTML!");
+} else if (topic === "CSS") {
+    console.log("Let's study CSS!");
+} else if (topic === "Git") {
+    console.log("Let's study Git!");
+} else if (topic === "Javascript") {
+    console.log("Let's study Javascript!");
+} else {
+    console.log("wah wah! Please try again");
+}
+*/
